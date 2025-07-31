e767961bdb1b8170877167ae67005e50df8727fe
RyanAugustoSchneider <schneider.ryan@escola.pr.gov.br>
2025-07-31 14:12:54 -0300

    Add files via upload

---
+++ flashcard-carla/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Matematica',
+    ' Qual número, quando você multiplica por qualquer outro, sempre mantém a identidade do outro número?',
+    'É o numero 1.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o relevo predominante no estado do paraná? ',
+    'É o relevo de planaltos.'
+)
+
+criaCartao(
+    'Jogos',
+    'Quais são os 3 jogos mais vendidos na steam?',
+    'Os três jogos mais vendidos na Steam atualmente são: Counter-Strike 2, Call of Duty: Black Ops 6 e Palworld. '
+)
+
+criaCartao(
+    'Computadores',
+    'Qual é o nome da peça responsável pela alimentação de energia dos demais componente de um computador?',
+    'É chamada de fonte de alimentação.  '
+)
+
+criaCartao(
+    'Roupas',
+    'qual é a marca que mais vende roupas no mundo?',
+    ' A marca que mais vende roupas no mundo, considerando valor de mercado e vendas, é a Nike.'
+)
